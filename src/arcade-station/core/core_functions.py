0ebca55dce3388ad63c09e28e5d5ffff91f7e9c7
Dean <dean@tamm.am>
2025-01-25 23:32:52 -0500

    Initial functions to create

---
+++ src/arcade-station/core/core_functions.py
@@ -0,0 +1,8 @@
+# function Get-Screenshot
+# function Invoke-SetMarqueeFromFile
+# function Open-Header
+# function Open-FullscreenImage
+# function Send-Keystrokes
+# function Set-ForegroundWindow
+# function Start-Sound
+# function Write-Log
