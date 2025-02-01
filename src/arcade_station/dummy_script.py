167caa57a4c0549f1125f20577340f1dd8f627ff
Dean <dean@tamm.am>
2025-02-01 18:23:47 -0500

    Dummy is smarter now

--- src/arcade_station/dummy_script.py
+++ src/arcade_station/dummy_script.py
@@ -1,5 +1,9 @@
+import sys
 import os
-from core.core_functions import open_header
+# Add the parent directory to the Python path to allow relative module imports
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
+
+from arcade_station.core.common.core_functions import *
 
 def main():
     """
