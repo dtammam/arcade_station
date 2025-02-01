3c3ed499db3cffb102a98d39c535a6f100e3fa5c
Dean <dean@tamm.am>
2025-02-01 18:17:57 -0500

    Update references and relative paths to import

--- src/arcade_station/kill_all_and_reset_pegasus.py
+++ src/arcade_station/kill_all_and_reset_pegasus.py
@@ -3,7 +3,7 @@ import os
 # Add the parent directory to the Python path to allow relative module imports
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
-from core.core_functions import *
+from arcade_station.core.common.core_functions import *
 
 kill_processes_from_toml('processes_to_kill.toml')
 start_pegasus()
--- src/arcade_station/listeners/key_listener.py
+++ src/arcade_station/listeners/key_listener.py
@@ -3,6 +3,6 @@ import os
 # Add the parent directory to the Python path to allow relative module imports
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
-from core.core_functions import *
+from arcade_station.core.common.core_functions import *
 
 start_listening_to_keybinds_from_toml('key_listener.toml') 
