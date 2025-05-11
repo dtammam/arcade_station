ec1cc4ed3b8d24e4e270db2fb738550a36da4e64
Dean <dean@tamm.am>
2025-05-10 21:48:03 -0400

    Remove reference to VBS files for screenshots

--- bin/windows/take_screenshot.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Hide console window completely
-if not "%1"=="hidden" start /b /min cmd /c %0 hidden & exit
-REM Run the screenshot script without showing a window
-start /b "Screenshot" "C:\Repositories\arcade_station\.venv\Scripts\pythonw.exe" "C:\Repositories\arcade_station\src\arcade_station\screenshot.py" 
